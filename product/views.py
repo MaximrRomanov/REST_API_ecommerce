8a1ecd4cdeea2a7a596ae7a02364fe077bc1a084
MaximrRomanov <maxprolife@icloud.com>
2023-09-21 12:22:48 +0300

    Добавил два представления для апи: для списка продуктов, для одного продукта

--- product/views.py
+++ product/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Product
+from .serializer import ProductSerializer
 
 
-# from .models import Product
-# Create your views here.
+class ProductListAPIView(ListCreateAPIView):
+    """
+    Может вывести список товаров при get-запросе,
+    может создать новый продукт при post-запросе
+
+    """
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Может обновить продукт в БД при put-запросе (update)
+    Может получить экземпляр объекта из БД при get-запросе (retrieve)
+    Может удалить продукт из БД при delete-запросе (destroy)
+
+    """
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
